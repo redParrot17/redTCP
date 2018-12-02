fab9f0f4c0051088f81e27227018faaf8f277552
redparrot17 <burnscr17@gcc.edu>
2018-12-02 18:36:51 -0500

    Added Connection#asServerConnection and Connection#asClientConnection along with a little more JavaDoc

--- src/main/java/listener_references/Connection.java
+++ src/main/java/listener_references/Connection.java
@@ -1,6 +1,7 @@
 package listener_references;
 
 import java.net.Socket;
+import java.util.Optional;
 
 /**
  * ServerConnection class that contains information about a socket connection to a client
@@ -11,13 +12,16 @@ public class Connection {
     private final long connectionCreation;
 
     /**
-     * @param socket    the actual {@link Socket} that the client is connected to
+     * @param socket the actual {@link Socket} that the client is connected to
      */
     public Connection(Socket socket) {
         connectionCreation = System.currentTimeMillis();
         this.socket = socket;
     }
 
+    /**
+     * @return the {@link Socket} associated with this connection
+     */
     public Socket getSocket() {
         return socket;
     }
@@ -26,6 +30,22 @@ public class Connection {
         return connectionCreation;
     }
 
-    public enum event {CONNECTED,REMOVED}
+    /**
+     * Attempts to wrap the object as a {@code ServerConnection}
+     * @return a {@link Optional<ServerConnection>} of this object wrapped as a {@code ServerConnection}
+     */
+    public Optional<ServerConnection> asServerConnection() {
+        return this instanceof ServerConnection ? Optional.of((ServerConnection) this) : Optional.empty();
+    }
+
+    /**
+     * Attempts to wrap the object as a {@code ClientConnection}
+     * @return a {@link Optional<ClientConnection>} of this object wrapped as a {@code ClientConnection}
+     */
+    public Optional<ClientConnection> asClientConnection() {
+        return this instanceof ClientConnection ? Optional.of((ClientConnection) this) : Optional.empty();
+    }
+
+    public enum Event {CONNECTED,REMOVED}
 
 }
