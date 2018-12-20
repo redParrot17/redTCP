9421e5797883ae66e4048620d6173d1e4af34760
redparrot17 <burnscr17@gcc.edu>
2018-12-20 01:45:31 -0500

    Updated JavaDoc Added ClientJsonListener support

--- src/main/java/client/ClientListenerManager.java
+++ src/main/java/client/ClientListenerManager.java
@@ -1,9 +1,10 @@
 package client;
 
-import listener_references.Command;
-import listener_references.Message;
-import listeners.CommandListener;
-import listeners.MessageListener;
+import listener_references.*;
+import listeners.ClientCommandListener;
+import listeners.ClientJsonListener;
+import listeners.ClientMessageListener;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,11 +13,15 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * A listener manager to manage all the event listeners
+ */
 public class ClientListenerManager {
 
     private ExecutorService executor;
-    private volatile List<MessageListener> messageListeners;
-    private volatile List<CommandListener> commandListeners;
+    private volatile List<ClientMessageListener> messageListeners;
+    private volatile List<ClientCommandListener> commandListeners;
+    private volatile List<ClientJsonListener> jsonListeners;
 
     /**
      * Constructs a new {@link ClientListenerManager} with no listeners pre-registered
@@ -25,42 +30,61 @@ public class ClientListenerManager {
         executor = Executors.newCachedThreadPool();
         messageListeners = new ArrayList<>();
         commandListeners = new ArrayList<>();
+        jsonListeners = new ArrayList<>();
     }
 
     /**
-     * Adds the specified {@link MessageListener} to the list
+     * Adds the specified {@link ClientMessageListener} to the list
      * @param listener the listener to be added
      */
-    public void addMessageListener(MessageListener listener) {
+    public void addMessageListener(ClientMessageListener listener) {
         Objects.requireNonNull(listener);
         messageListeners.add(listener);
     }
 
     /**
-     * Removes the specified {@link MessageListener} from the list
+     * Removes the specified {@link ClientMessageListener} from the list
      * @param listener the listener to be removed
      */
-    public void removeMessageListener(MessageListener listener) {
+    public void removeMessageListener(ClientMessageListener listener) {
         Objects.requireNonNull(listener);
         messageListeners.remove(listener);
     }
 
     /**
-     * Adds the specified {@link CommandListener} to the list
+     * Adds the specified {@link ClientCommandListener} to the list
      * @param listener the listener to be added
      */
-    public void addCommandListener(CommandListener listener) {
+    public void addCommandListener(ClientCommandListener listener) {
         Objects.requireNonNull(listener);
         commandListeners.add(listener);
     }
 
     /**
-     * Removes the specified {@link CommandListener} from the list
+     * Removes the specified {@link ClientCommandListener} from the list
      * @param listener the listener to be removed
      */
-    public void removeCommandListener(CommandListener listener) {
+    public void removeCommandListener(ClientCommandListener listener) {
         Objects.requireNonNull(listener);
         commandListeners.remove(listener);
+    }
+
+    /**
+     * Adds the specified {@link ClientJsonListener} to the list
+     * @param listener the listener to be added
+     */
+    void addJsonListener(ClientJsonListener listener) {
+        Objects.requireNonNull(listener);
+        jsonListeners.add(listener);
+    }
+
+    /**
+     * Removes the specified {@link ClientJsonListener} from the list
+     * @param listener the listener to be removed
+     */
+    void removeJsonListener(ClientJsonListener listener) {
+        Objects.requireNonNull(listener);
+        jsonListeners.remove(listener);
     }
 
     /**
@@ -69,13 +93,14 @@ public class ClientListenerManager {
     public void removeAllListeners() {
         messageListeners.clear();
         commandListeners.clear();
+        jsonListeners.clear();
     }
 
     /**
-     * Runs each of the {@link MessageListener}s with the {@code message} as input
-     * @param message the {@link Message} to pass to each of the listeners
+     * Runs each of the {@link ClientMessageListener}s with the {@code message} as input
+     * @param message the {@link MessageImpl} to pass to each of the listeners
      */
-    public synchronized void raiseMessageEvent(Message message) {
+    synchronized void raiseMessageEvent(ClientMessage message) {
         messageListeners.forEach(listener -> executor.submit((Callable<Void>) () -> {
             listener.onMessageReceived(message);
             return null;
@@ -83,12 +108,23 @@ public class ClientListenerManager {
     }
 
     /**
-     * Runs each of the {@link CommandListener}s with the {@code command} as input
-     * @param command the {@link Command} to pass to each of the listeners
+     * Runs each of the {@link ClientCommandListener}s with the {@code command} as input
+     * @param command the {@link CommandImpl} to pass to each of the listeners
      */
-    public synchronized void raiseCommandEvent(Command command) {
+    synchronized void raiseCommandEvent(ClientCommand command) {
         commandListeners.forEach(listener -> executor.submit((Callable<Void>) () -> {
             listener.onCommandReceived(command);
+            return null;
+        }));
+    }
+
+    /**
+     * Runs each of the {@link ClientJsonListener}s with the {@code command} as input
+     * @param json the {@link JSONObject} to pass to each of the listeners
+     */
+    synchronized void raiseJsonEvent(ClientJson json) {
+        jsonListeners.forEach(listener -> executor.submit((Callable<Void>) () -> {
+            listener.onJsonReceived(json);
             return null;
         }));
     }
